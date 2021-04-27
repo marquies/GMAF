a84d090f4491d87a2dab1960b311a064bd6aa306
Patrick Steinert <me@patrickbreucking.de>
2021-04-27 09:43:56 +0200

    Modified Test class to run without graph.xml

--- src/main/java/de/swa/gmaf/Test.java
+++ src/main/java/de/swa/gmaf/Test.java
@@ -7,37 +7,40 @@ import de.swa.gc.GraphCodeGenerator;
 import de.swa.mfv.FeatureVector;
 import de.swa.mfv.builder.FeatureVectorBuilder;
 import de.swa.mfv.builder.GraphMLFlattener;
-import de.swa.mfv.builder.ImageFlattener;
 import de.swa.mfv.builder.JsonFlattener;
 import de.swa.mfv.builder.XMLEncodeDecode;
 
 public class Test {
 	//  NOTE: add envorionment entry: GOOGLE_APPLICATION_CREDENTIALS -> /Users/stefan_wagenpfeil/eclipse-workspace/gmaf/API_Keys/Diss-0e43fbd15daa.json
 	public static void main(String[] args) throws Exception {
-		RandomAccessFile rf = new RandomAccessFile("graph.xml", "r");
-		String line = "";
-		String content = "";
-		while ((line = rf.readLine()) != null) {
-			content += line;
-		}
-		rf.close();
+		RandomAccessFile rf;
+//		rf = new RandomAccessFile("graph.xml", "r");
+//		String line = "";
+//		String content = "";
+//		while ((line = rf.readLine()) != null) {
+//			content += line;
+//		}
+//		rf.close();
 		String s = "";
-		
-		FeatureVector fv = FeatureVectorBuilder.unflatten(content, new XMLEncodeDecode());
+
+//		FeatureVector fv = FeatureVectorBuilder.unflatten(content, new XMLEncodeDecode());
+
+		FeatureVector fv = new FeatureVector();
+
 		ApiFacade af = new ApiFacade();
-		
+
 		String testFile = "Rome.jpg";
-		FileInputStream fs = new FileInputStream("Rome.jpg");
+		FileInputStream fs = new FileInputStream("../Gruppe4/0600.png");
 		byte[] bytes = fs.readAllBytes();
 		fv = af.processImage(bytes, testFile, "sw", 2);
-		
+
 		s = FeatureVectorBuilder.flatten(fv, new GraphMLFlattener());
-		
+
 		rf = new RandomAccessFile("graph.graphml", "rw");
 		rf.setLength(0);
 		rf.writeBytes(s);
 		rf.close();
-		
+
 		s = FeatureVectorBuilder.flatten(fv, new GraphCodeGenerator());
 //		if (true) return;
 
@@ -46,7 +49,7 @@ public class Test {
 		rf.setLength(0);
 		rf.writeBytes(s);
 		rf.close();
-		
+
 		s = FeatureVectorBuilder.flatten(fv, new XMLEncodeDecode());
 		rf = new RandomAccessFile("graph.xml", "rw");
 		rf.setLength(0);
